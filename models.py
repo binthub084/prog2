0074709e1d3142bf733e2bd874faf722959490dd
Akiyama Satoshi <j3303sxj@sit.ac.jp>
2024-12-17 16:33:47 +0900

    修正

--- models.py
+++ models.py
@@ -1,61 +1,68 @@
+import torch
 from torch import nn
+
 
 class MyModel(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.flatten = nn.Flatten()
-        self.network = nn.Sepuential(
-            nn.Linear(28*28,512),
+        self.network = nn.Sequential(
+            nn.Linear(28*28, 512),
             nn.ReLU(),
-            nn.Linear(512,512),
+            nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(512,10)
+            nn.Linear(512, 10),
         )
 
     def forward(self, x):
         x = self.flatten(x)
         logits = self.network(x)
         return logits
-    
-def train(model,dataloader,loss_fn,optimizer):
 
+
+def test_accuracy(model, dataloader):
+    n_corrects = 0  # 正解の個数
+
+    model.eval()
+    for image_batch, label_batch in dataloader:
+        with torch.no_grad():
+            logits_batch = model(image_batch)
+
+        predict_batch = logits_batch.argmax(dim=1)
+        n_corrects += (label_batch == predict_batch).sum().item()
+
+    accuracy = n_corrects / len(dataloader.dataset)
+    return accuracy
+
+
+def train(model, dataloader, loss_fn, optimizer):
+    """1 epoch の学習"""
     model.train()
-    for image_batch,label_batch in dataloader:
-
+    for image_batch, label_batch in dataloader:
         logits_batch = model(image_batch)
 
-        loss=loss_fn(logits_batch,label_batch)
+        loss = loss_fn(logits_batch, label_batch)
 
+        # 最適化
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        loss.backward()  # 誤差逆伝播法 (back propagation)
+        optimizer.step()  # パラメーターをちょっと動かす
 
-    return loss.list()
+    # 最後のバッチのロス
+    return loss.item()
 
-acc_test=models.test_accuracy(model,dataloader_test)
-print(f'test accuracy: {acc_test*100:.2f}%')
-
-n_epochs=5
-
-for k in range(n_epochs):
-    print(f'epoch {k+1}/{n_epochs}',end=': ',flush=True)
-
-    loss_train=models.train(model,dataloader_train,loss_fn,optimizer)
-    print(f'train loss:{loss_train}')
-    acc_test=models.test_accuracy(model,dataloader_test)
-    print(f'test accuracy:{acc_test*100:.2f}%')
 
 def test(model, dataloader, loss_fn):
-    loss_total = 0.0  
+    loss_total = 0.0
 
-    model.eval()  
-    for image_batch, label_batch in dataloader:  
-        with torch.no_grad():  
-            logits_batch = model(image_batch)  
+    model.eval()
+    for image_batch, label_batch in dataloader:
+        with torch.no_grad():
+            logits_batch = model(image_batch)
 
-            loss = loss_fn(logits_batch, label_batch)
-            loss_total += loss.item()  
+        loss = loss_fn(logits_batch, label_batch)
+        loss_total += loss.item()
 
+    # バッチ数で割って、平均値を返す
     return loss_total / len(dataloader)
-
